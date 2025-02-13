5894cec3085cdd9ebbe012883a56c7386d69462a
cl7-mb1 <milanb@codela7.com>
2025-02-13 12:26:48 +0530

    commit and push

--- require  and require_once.php
+++ require  and require_once.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
 <html>
 <body>
-
 <h1> welcome to new page</h1>
-
-<?php include('script.php') ;
+<?php
+require ('script.php');
 {
-
     echo "<br>how are you!";
 }
-include_once  ('script.php') ;
+require_once ('script.php');
 {
-
     echo "<br>how are you!";
 }
+
 ?>
 </body>
 </html>
-
-
