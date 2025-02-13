18b9433eef3f0ad149c9f59940718c4aa97f1aed
cl7-mb1 <milanb@codela7.com>
2025-02-13 19:50:16 +0530

    array

---
+++ key-arry.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$number = [
+	"milan" => "12",
+	"parth" => "13",
+	"zeel"  => "14"
+];
+
+echo "this name " . $number['milan'] . ".";
+?>
+<?php
+$name = [
+		"milan",
+		"jeel",
+		"parth"
+];
+echo "name" .$name[0].".";
+?>
+</html>
+</body>
