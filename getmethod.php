2a7147506e63f11fdf6a9561f5210870d446d9c5
cl7-mb1 <milanb@codela7.com>
2025-02-14 14:51:12 +0530

    get method

---
+++ getmethod.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="getmethod.php" method="GET">
+    Name: <input type="text" name="name">
+    Email: <input type="text" name="email">
+    <input type="submit">
+</form>
+<?php
+if (isset($_GET['name']) && isset($_GET['email'])) {
+    $name = htmlspecialchars($_GET['name']);
+    $email = htmlspecialchars($_GET['email']);
+    echo "Name: $name<br>";
+    echo "Email: $email";
+} else {
+    echo "Please provide both name and email.";
+}
+?>
+</body>
+</html>
+
+
