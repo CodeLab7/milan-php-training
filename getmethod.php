d000d2aee7a2591602ca56d32a12726938791da8
cl7-mb1 <milanb@codela7.com>
2025-02-14 14:56:08 +0530

    get method

--- getmethod.php
+++ getmethod.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form action="getmethod.php" method="GET">
-    Name: <input type="text" name="name">
-    Email: <input type="text" name="email">
-    <input type="submit">
+<form>
+    <form action="getmethod.php" method="GET">
+        Name: <input type="text" name="name">
+        Email: <input type="text" name="email">
+        <input type="submit">
+    </form>
+
 </form>
 <?php
 if (isset($_GET['name']) && isset($_GET['email'])) {
