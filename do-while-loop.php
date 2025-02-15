551e545bdd4868ccdf2ac572571f4c318e56fef1
cl7-ks1 <kalpeshs@codelab7.com>
2025-02-15 11:36:32 +0530

    understanding loop

--- do-while-loop.php
+++ do-while-loop.php
@@ -2,7 +2,23 @@
 $i = 1;
 
 do {
-    echo $i;
-    $i++;
-} while ($i < 6);
+	echo $i . "<br>";
+	$i++;
+} while($i <= 10);
+
+// Output: 1 2 3 4 5 6 7 8 9 10
+
+$names = ["John", "David", "Amy"];
+
+do {
+	echo current($names) . "<br>";
+} while(next($names));
+
+$i = 1;
+
+do {
+	$i++;
+	echo $i != 6  . "<br>";;
+} while($i != 6);
+
 ?>
