92f2526bcd91da608bf505d570bb96c6f49f8c67
cl7-mb1 <milanb@codela7.com>
2025-02-17 16:55:14 +0530

    error soul

---
+++ put-file-content-fun.php
@@ -0,0 +1,14 @@
+<?php
+$file = 'D:\code\milan-php-training\script.php';
+$data = 'hello , wrold!';
+
+$result = file_put_contents($file,$data);
+if ($result === false){
+    echo "error writing to file";
+}else{
+    echo "data write to the file successfuly";
+}
+?>
+
+<!--output 1 main file: data write the file successfuly-->
+<!--output 2 script file: 'hello , world!' -->
