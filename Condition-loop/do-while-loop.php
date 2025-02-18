3a47a92b754aee1458a168019de97ae1c244fe36
Parth Sutariya <pathusutariya@gmail.com>
2025-02-18 18:44:40 +0530

    Update examples

--- Condition-loop/do-while-loop.php
+++ Condition-loop/do-while-loop.php
@@ -1,25 +1,39 @@
 <?php
+
+/**
+ * Simple example to print 10 Numbers using do-while loop
+ * Output: 1 2 3 4 5 6 7 8 9 10
+ */
+
 $i = 1;
-
 do {
-	echo $i . "<br>";
-	$i++;
-} while($i <= 10);
+    echo $i . "<br>";
+    $i++;
+} while ($i <= 10);
 
-// Output: 1 2 3 4 5 6 7 8 9 10
+
+
+/**
+ * Simple example to print 3 Names using do-while loop
+ * Output: John David Amy
+ */
 
 $names = ["John", "David", "Amy"];
 
 do {
-	echo current($names) . "<br>";
-} while(next($names));
+    echo current($names) . "<br>";
+} while (next($names));
 
+
+/**
+ * Simple example to print 1 for 6 time using do-while loop
+ * Output: 1
+ */
 $i = 1;
-
 do {
-	$i++;
-	echo $i != 6  . "<br>";;
-} while($i != 6);
+    echo $i . "<br>";
+    $i++;
+} while ($i != 6);
 
 // output: 1 1 1 1 1
 ?>
