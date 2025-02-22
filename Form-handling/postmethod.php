0d2652e7588ca4b1097db8aac643668b1e7d1bf4
cl7-mb1 <milanb@codela7.com>
2025-02-22 12:45:45 +0530

    form tage ni andar form tage

--- Form-handling/postmethod.php
+++ Form-handling/postmethod.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form>
+
     <form action="postmethod.php" method="POST">
         Name: <input type="text" name="name">
         Email: <input type="text" name="email">
         <input type="submit">
     </form>
 
-</form>
+
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "post"){
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
-    echo "Name : $name<br>";
+    echo "Name :$name<br>";
     echo "Email : $email";
 }
 
