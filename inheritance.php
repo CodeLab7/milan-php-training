7ea0be9cecb11f4326edfca970cf47bf3df47890
cl7-mb1 <milanb@codelab7.com>
2025-06-17 10:18:23 +0530

    example update

--- inheritance.php
+++ inheritance.php
@@ -1,38 +1,8 @@
-<!-- inheritance example
-    output: Am i a frute or berry? The frute is Strawberry and the color is Red. -->
+
 
 <!DOCTYPE html>
 <html>
 <body>
-
-<?php
-class Frute{
-    public $name;
-    public $color;
-    function __construct($name, $color){
-        $this->name = $name;
-        $this->color = $color;
-    }
-    public function intro(){
-        echo "The frute is {$this->name} and the color is {$this->color}.";
-    }
-}
-// Strawberry is inherited from Frute
-class Strawberry extends Frute{
-    public function message() {
-        echo "Am i a frute or berry? ";
-    }
-}
-$strawberry = new Strawberry("Strawberry" , "Red") ;
-$strawberry->message();
-$strawberry->intro() ;
-echo "<br>"
-?>
-</body>
-</html>
-
-<!-- overriding inheritance  in example-->
-<!-- output: The company name is Hyundai and name is Verna in color Black.-->
 
 <?php
 class Car{
@@ -63,3 +33,5 @@ $verna = new Verna("Hyundai","Verna", "Black");
 $verna->intro();
 
 ?>
+</body>
+</html>
