c12bd8b45b6a6c0aa05920a1069cd0ed67c316e1
cl7-mb1 <milanb@codelab7.com>
2025-06-17 09:47:51 +0530

    example update

--- class.php
+++ class.php
@@ -1,28 +1,4 @@
-<?php
-class Frute {
-    private $name;
-    private $color;
-
-function set_name($name){
-    $this->name = $name;
-}
-function get_name(){
-    return $this->name;
-}
-function set_color($color){
-    $this->color = $color;
-}
-function get_color(){
-    return $this->color;
-}
-}
-
-$Frute = new frute ();
-$Frute->set_color('Apple');
-$Frute->set_name('Green');
-echo $Frute->get_color();
-echo $Frute->get_name() . "<br>";
-?>
+<h1> example </h1>
 
 <?php
 // parent class
@@ -34,10 +10,7 @@ abstract class Car{
 
 }
 
-
 // child class
-// outpt:choose german quality ! Audi!
-
 class Audi extends Car {
     public function intro() : String {
         return " choose german quality ! $this->name! ";
