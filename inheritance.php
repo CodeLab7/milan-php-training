41a26bb13491662d2c2c2f576f58c26d2d030e96
cl7-ks1 <kalpeshs@codelab7.com>
2025-06-18 09:53:01 +0530

    =improve example

--- inheritance.php
+++ inheritance.php
@@ -1,35 +1,42 @@
-
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <body>
 
 <?php
-class Car{
-    public $make;
-    public $name;
-   public function __construct($make, $name) {
-       $this->make = $make;
-       $this->name = $name;
-       }
-       public function intro(){
-        echo "The company name is {$this->make} and  name is {$this->name}.";
-    }
-}
 
-class Verna extends Car{
-    public $color;
-    public function __construct($make, $name, $color ) {
-        $this->make = $make;
-        $this->name = $name;
-        $this->color =$color;
-    }
-    public function intro(){
-        echo "The company name is {$this->make} and name is   {$this->name} in color  {$this->color}.";
-    }
+class Car {
+
+	public $make;
+	public $name;
+
+	public function __construct($make, $name) {
+		$this->make = $make;
+		$this->name = $name;
+	}
+
+	public function company() {
+		echo "The company name is {$this->make} and  name is {$this->name}.";
+	}
 
 }
-$verna = new Verna("Hyundai","Verna", "Black");
+
+class Verna extends Car {
+
+	public $color;
+
+	public function __construct($make, $name, $color) {
+		parent::__construct($make, $name);
+		$this->color = $color;
+	}
+
+	public function intro() {
+		echo $this->company() . " in color  {$this->color}.";
+	}
+
+}
+
+$verna = new Verna("Hyundai", "Verna", "Black");
+
 $verna->intro();
 
 ?>
