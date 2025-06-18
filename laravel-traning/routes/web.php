7f076119ef1f01b156d4c5179b5a1730977140e4
cl7-ks1 <kalpeshs@codelab7.com>
2025-06-18 10:26:45 +0530

    remove unnecesary code

--- laravel-traning/routes/web.php
+++ laravel-traning/routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-  // URL Genration
- // Route::view('home','home');
- // Route::view('user','home');
-
- // Route::view('about','about');
- // Route::view('about/{name}','about');
-
-Route::post('/login',[app\Http\controller
