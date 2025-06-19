6885c4d54b78847a35fb3e33a4212f275e433997
cl7-mb1 <milanb@codelab7.com>
2025-06-19 11:03:45 +0530

    web route example

--- laravel-traning/routes/web.php
+++ laravel-traning/routes/web.php
@@ -5,3 +5,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Open the Users page to view all users.
+Route::get('/users', [UserController::class, 'index']);
+//Show single user
+Route::get('/users/{id}', [UserController::class, 'show']);
+
+//Submit a form to create a new user.
+Route::post('/users', [UserController::class, 'store']);
+
+//Submit an edit form to update all user fields.
+Route::put('/users/{id}', [UserController::class, 'update']);
+
+//Just change the user's status (active/inactive) without editing full details.
+Route::patch('/users/{id}/status', [UserController::class, 'updateStatus']);
+
+//Permanently remove a user.
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// prefix group example
+Route::prefix('users')->group(function () {
+	// Open the Users page to view all users.
+	Route::get('/', [UserController::class, 'index']);
+
+	// Show single user
+	Route::get('/{id}', [UserController::class, 'show']);
+
+	// Submit a form to create a new user.
+	Route::post('/', [UserController::class, 'store']);
+
+	// Submit an edit form to update all user fields.
+	Route::put('/{id}', [UserController::class, 'update']);
+
+	// Just change the user's status (active/inactive) without editing full details.
+	Route::patch('/{id}/status', [UserController::class, 'updateStatus']);
+
+	// Permanently remove a user.
+	Route::delete('/{id}', [UserController::class, 'destroy']);
+});
+
+// Route Name
+Route::get('user/profile', fn() => 'User Profile')->name('profile');
