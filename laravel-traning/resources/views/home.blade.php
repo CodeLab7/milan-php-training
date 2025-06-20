9d1b7730a14a4464569b428135dae5f05f8ecfac
cl7-mb1 <milanb@codelab7.com>
2025-06-20 15:09:23 +0530

    route method working

--- laravel-traning/resources/views/home.blade.php
+++ laravel-traning/resources/views/home.blade.php
@@ -1,11 +1,10 @@
 <div>
-	<h1>Home Page</h1> -->
-	<h3>{{URL::current()}}</h3>
-    <h3>{{URL::full()}}</h3>
+	<h1>Home Page</h1>
 
 
 	 <h3>{{url()->current()}}</h3>
     <h3>{{url()->full()}}</h3>
     <a href="{{URL::to('about')}}" >About Page</a>
     <a href="{{URL::to('about',['Milan'])}}" >About Page</a>
+	<a href="{{route('user.single',1)}}" >About Page</a>
 </div>
