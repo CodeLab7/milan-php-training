aa9aa6255a6225bc587d26f94c798146cb87c1ac
cl7-mb1 <milanb@codelab7.com>
2025-06-20 14:41:53 +0530

    usercontroller name comman in prefix

--- laravel-traning/routes/web.php
+++ laravel-traning/routes/web.php
@@ -3,9 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
-
 
 // Open the Users page to view all users.
 Route::get('/users', [UserController::class, 'index']);
@@ -25,24 +24,16 @@ Route::patch('/users/{id}/status', [UserController::class, 'updateStatus']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 // prefix group example
-Route::prefix('users')->group(function () {
-	// Open the Users page to view all users.
-	Route::get('/', [UserController::class, 'index']);
-
-	// Show single user
-	Route::get('/{id}', [UserController::class, 'show']);
-
-	// Submit a form to create a new user.
-	Route::post('/', [UserController::class, 'store']);
-
-	// Submit an edit form to update all user fields.
-	Route::put('/{id}', [UserController::class, 'update']);
-
-	// Just change the user's status (active/inactive) without editing full details.
-	Route::patch('/{id}/status', [UserController::class, 'updateStatus']);
-
-	// Permanently remove a user.
-	Route::delete('/{id}', [UserController::class, 'destroy']);
+Route::prefix('users')
+     ->controller(UserController::class)
+     ->name('user.')
+     ->group(function () {
+	     Route::get('/', 'index');
+	     Route::post('/store', 'store');
+	     Route::get('/{user}', 'single');
+	     Route::put('/{user}/update', 'update');
+	     Route::patch('/{user}/update/status', 'updateStatus');
+	     Route::delete('/{user}/delete', 'destroy');
 });
 
 // Route Name
