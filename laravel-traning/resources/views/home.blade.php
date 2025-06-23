93378b93928d55222b9368d534450e3023c05217
Nidhi V <155521293+cl7-nv1@users.noreply.github.com>
2025-06-23 17:30:06 +0530

    code formatting

--- laravel-traning/resources/views/home.blade.php
+++ laravel-traning/resources/views/home.blade.php
@@ -1,8 +1,6 @@
 <div>
 	<h1>Home Page</h1>
-
-
-	 <h3>{{url()->current()}}</h3>
+	<h3>{{url()->current()}}</h3>
     <h3>{{url()->full()}}</h3>
     <a href="{{URL::to('about')}}" >About Page</a>
     <a href="{{URL::to('about',['Milan'])}}" >About Page</a>
