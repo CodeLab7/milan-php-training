c8443acf0a62f21d8a2a7de184d0979ed899a62f
cl7-mb1 <milanb@codelab7.com>
2025-07-07 15:08:05 +0530

    seeding file dummy deta

---
+++ laravel-traning/database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+
+    public function run(): void
+    {
+
+	    User::create([
+		    'name' => 'rohan ',
+		    'phone_no' => '6353479965',
+		    'email' => 'rohan@gmail.com',
+		    'password' => bcrypt('password124'),
+	    ]);
+
+    }
+}
