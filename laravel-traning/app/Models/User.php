d893c2cd1a937278b78c96e2e6a3741aca7eec0b
cl7-mb1 <milanb@codelab7.com>
2025-07-08 14:55:32 +0530

    code formatting

--- laravel-traning/app/Models/User.php
+++ laravel-traning/app/Models/User.php
@@ -1,49 +1,52 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+class User extends Authenticatable {
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'phone_no',
-        'email',
-        'password',
-    ];
+	use HasFactory;
+	protected $fillable = [
+		'name',
+		'phone_no',
+		'email',
+		'password',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	protected function name(): attribute {
+		return Attribute::make(
+			set: fn($value) => strtoupper($value),
+			get: fn($value) => ucfirst(strtolower($value))
+		);
+	}
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+	protected function password(): Attribute {
+		return Attribute::make(
+			set: fn($value) => bcrypt($value),
+
+		);
+	}
+
+	protected $casts  = [
+		'phone_no' => 'string',
+	];
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
+
+	/**
+	 * Get the attributes that should be cast.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function casts(): array {
+		return [
+			'email_verified_at' => 'datetime',
+			'password'          => 'hashed',
+		];
+	}
+
 }
