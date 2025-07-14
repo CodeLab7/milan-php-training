4e41ef94fa0c4a5e423fc66b4e7e20325803565a
cl7-mb1 <milanb@codelab7.com>
2025-07-14 17:11:11 +0530

    code formating

--- laravel-traning/app/Http/Controllers/UserController.php
+++ laravel-traning/app/Http/Controllers/UserController.php
@@ -7,39 +7,38 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller {
 
-
 	public function single($id) {
 		return view('user.single', ['id' => $id]);
 	}
-	public function index()
-	{
+
+	public function index() {
 		$users = User::all();
 		return view('user.index', compact('users'));
-
 	}
+
 	public function create() {
 		return view('user.create');
 	}
 
-	public function store(Request $request)
-	{
+	public function store(Request $request) {
 
 		$validated = $request->validate([
 			'username' => 'required|string|max:255',
 			'phone_no' => 'nullable|string|max:20',
-			'email' => 'required|email|unique:users,email',
-			'address' => 'nullable|string|max:255',
+			'email'    => 'required|email|unique:users,email',
+			'address'  => 'nullable|string|max:255',
 			'password' => 'required|min:6',
 		]);
 
 		User::create([
-			'name' => $validated['username'],
+			'name'     => $validated['username'],
 			'phone_no' => $validated['phone_no'],
-			'email' => $validated['email'],
-			'address' => $validated['address'],
+			'email'    => $validated['email'],
+			'address'  => $validated['address'],
 			'password' => bcrypt($validated['password']),
 		]);
 
 		return redirect()->route('user.index')->with('success', 'User created successfully!');
 	}
+
 }
--- laravel-traning/app/View/Components/Form/Input.php
+++ laravel-traning/app/View/Components/Form/Input.php
@@ -4,25 +4,24 @@ namespace App\View\Components\Form;
 
 use Illuminate\View\Component;
 
-class Input extends Component
-{
+class Input extends Component {
+
 	public $name;
 	public $label;
 	public $type;
 	public $placeholder;
 	public $class;
 
-	public function __construct($name, $label = '', $type = 'text', $placeholder = '', $class = '')
-	{
-		$this->name = $name;
-		$this->label = $label;
-		$this->type = $type;
+	public function __construct($name, $label = '', $type = 'text', $placeholder = '', $class = '') {
+		$this->name        = $name;
+		$this->label       = $label;
+		$this->type        = $type;
 		$this->placeholder = $placeholder;
-		$this->class = $class;
+		$this->class       = $class;
 	}
 
-	public function render()
-	{
+	public function render() {
 		return view('components.form.input');
 	}
+
 }
