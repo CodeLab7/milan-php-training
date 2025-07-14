1a751dc82d9893c851223cc957b9f63dddd1f275
cl7-mb1 <milanb@codelab7.com>
2025-07-14 17:21:24 +0530

    dummydataseeder ,code formating

--- laravel-traning/database/seeders/DatabaseSeeder.php
+++ laravel-traning/database/seeders/DatabaseSeeder.php
@@ -3,22 +3,28 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-         User::factory()->count(10)->create();
+class DatabaseSeeder extends Seeder {
 
-	    User::factory()->create([
-		    'name' => 'Test User',
-		    'phone_no' => fake()->phoneNumber(),
-		    'email' => 'test@email.com',
-	    ]);
-    }
+	/**
+	 * Seed the application's database.
+	 */
+	public function run(): void {
+
+		User::factory()->create([
+			'name'     => 'Test User',
+			'phone_no' => fake()->phoneNumber(),
+			'email'    => 'test@email.com',
+		]);
+		User::create([
+			'name'     => 'rohan ',
+			'phone_no' => '6353479965',
+			'email'    => 'rohan@email.com',
+			'password' => bcrypt('password124'),
+		]);
+	}
+
 }
