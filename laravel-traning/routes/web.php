e20f8466b779bc155b1e3a74ad323a25a5d3f7f4
cl7-mb1 <milanb@codelab7.com>
2025-07-25 18:13:41 +0530

    auth

--- laravel-traning/routes/web.php
+++ laravel-traning/routes/web.php
@@ -1,49 +1,20 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// prefix group example
-Route::prefix('users')
-     ->controller(UserController::class)
-     ->name('user.')
-     ->group(function () {
-	     Route::get('/', 'index')->name('index');
-	     Route::post('/store', 'store')->name('store');
-	     Route::get('/create/form', 'create')->name('create');
-	     Route::get('/{user}/single', 'single')->name('single');
-	     Route::get('/{user}/download', 'download')->name('download');
-	     Route::put('/{user}/update', 'update')->name('update');
-	     Route::patch('/{user}/update/status', 'updateStatus')->name('updateStatus');
-	     Route::delete('/{user}/delete', 'destroy')->name('destroy');
-     });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// URL Generation
-Route::view('home', 'home');
-Route::view('user', 'home');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::view('about', 'about');
-Route::view('about/{name}', 'about');
-
-/*// Open the Users page to view all users.
-Route::get('/users', [UserController::class, 'index']);
-//Show single user
-Route::get('/users/{id}', [UserController::class, 'show']);
-
-//Submit a form to create a new user.
-Route::post('/users', [UserController::class, 'store']);
-
-//Submit an edit form to update all user fields.
-Route::put('/users/{id}', [UserController::class, 'update']);
-
-//Just change the user's status (active/inactive) without editing full details.
-Route::patch('/users/{id}/status', [UserController::class, 'updateStatus']);
-
-//Permanently remove a user.
-Route::delete('/users/{id}', [UserController::class, 'destroy']);*/
-
-// Route Name
-Route::get('user/profile', fn() => 'User Profile')->name('profile');
+require __DIR__.'/auth.php';
