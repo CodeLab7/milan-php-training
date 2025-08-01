9cc46e7fb08fba9f239b79c4540dbc4aedb35cfc
cl7-mb1 <milanb@codelab7.com>
2025-08-01 09:32:24 +0530

    update route

--- laravel-traning/routes/api.php
+++ laravel-traning/routes/api.php
@@ -8,7 +8,12 @@ Route::get('/user', function (Request $request) {
 	return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/users', [UserApiController::class, 'index']);
-Route::post('/users/register', [UserApiController::class, 'register']);
-Route::post('/users/{id}/update', [UserApiController::class, 'update']);
-Route::delete('/users/{id}/delete', [UserApiController::class, 'destroy']);
+Route::prefix('users')
+	->controller(UserApiController::class)
+	->name('user.')
+	->group(function () {
+		Route::get('/', 'index')->name('index');
+		Route::post('/register', 'register')->name('register');
+		Route::post('/{id}/update', 'update')->name('update');
+		Route::delete('/{id}/delete', 'destroy')->name('destroy');
+	});
