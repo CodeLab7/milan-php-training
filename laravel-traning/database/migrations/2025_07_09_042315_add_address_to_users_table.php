12cf38e0c61ea1c6bf25bef12be9bc587f130769
cl7-mb1 <milanb@codelab7.com>
2025-08-04 15:51:57 +0530

    minor change

--- laravel-traning/database/migrations/2025_07_09_042315_add_address_to_users_table.php
+++ laravel-traning/database/migrations/2025_07_09_042315_add_address_to_users_table.php
@@ -12,9 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-			$table->string('address')
-				->nullable()
-				->after('email');
+			$table->string('address')->nullable()->after('email');
 
         });
     }
