9cf1eb0f6cd591eae9aa65426a24b4c2d0aab583
cl7-mb1 <milanb@codelab7.com>
2025-08-04 15:26:42 +0530

    attributes add

--- laravel-traning/resources/views/components/form/input.blade.php
+++ laravel-traning/resources/views/components/form/input.blade.php
@@ -1,18 +1,12 @@
-<div class="mb-3">
+<div class="form-group ">
     @if($label)
-        <label for="{{ $name }}" class="form-label">{{ $label }}</label>
+        <label class="form-label">{{ $label }}</label>
     @endif
 
-    <input
-            type="{{ $type }}"
-            id="{{ $name }}"
-            name="{{ $name }}"
-            placeholder="{{ $placeholder }}"
-            class="form-control"
-            value="{{ old($name) }}"
-    >
-    {{$slot}}
-        @error($name)
-        <div class="invalid-feedback">{{ $message }}</div>
-        @enderror
+    <input {{$attributes->merge(['class' => 'form-control'])}} />
+
+    {{ $slot }}
+    @error($attributes->get('name'))
+    <div class="invalid-feedback">{{ $message }}</div>
+    @enderror
 </div>
