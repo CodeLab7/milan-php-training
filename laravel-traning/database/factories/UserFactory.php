aa3186653682213f7a5633244e1a6672eb430915
cl7-mb1 <milanb@codelab7.com>
2025-08-04 15:22:39 +0530

    minor code change

--- laravel-traning/database/factories/UserFactory.php
+++ laravel-traning/database/factories/UserFactory.php
@@ -9,36 +9,37 @@ use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
-{
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+class UserFactory extends Factory {
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
+	/**
+	 * The current password being used by the factory.
+	 */
+	protected static ?string $password;
 
-	    return [
-		    'name' => $this->faker->name(),
-		    'email' => $this->faker->unique()->safeEmail(),
-		    'phone_no' => $this->faker->phoneNumber(),
-		    'password' => bcrypt('password'),
-	    ];
-    }
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition(): array {
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+		return [
+			'name'              => $this->faker->name(),
+			'email'             => $this->faker->unique()->safeEmail(),
+			'email_verified_at' => now(),
+			'phone_no'          => $this->faker->phoneNumber(),
+			'password'          => bcrypt('password'),
+			'remember_token'    => Str::random(10),
+		];
+	}
+
+	/**
+	 * Indicate that the model's email address should be unverified.
+	 */
+	public function unverified(): static {
+		return $this->state(fn(array $attributes) => [
+			'email_verified_at' => NULL,
+		]);
+	}
+
 }
